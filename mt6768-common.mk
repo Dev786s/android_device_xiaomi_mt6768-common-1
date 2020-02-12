976e0cfe1681696bb6f7fb808fefad7725820b2e
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2020-02-12 01:51:30 +0000

    mt6768-common: Enable freeform window management
    Change-Id: I2de7aab9d42c33989b53052266350a45982273db

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -327,3 +327,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.1-service.mt6768 \
     android.hardware.usb.gadget@1.1-service.mt6768
+
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
