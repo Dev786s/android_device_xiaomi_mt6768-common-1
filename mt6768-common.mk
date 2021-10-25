31f90e18dea16bf4abdf2f12427c39988292595b
fourninesix <klmajor2213@gmail.com>
2021-10-25 17:08:09 +0000

    mt6768-common: Build few more audio blobs from source
    Signed-off-by: fourninesix <klmajor2213@gmail.com>

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -32,6 +32,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0.vendor \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
+    android.hardware.audio.common-util.vendor \
+    android.hardware.audio.common@6.0-util.vendor \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.common@6.0-util \
     android.hardware.soundtrigger@2.2 \
