a4451f64dba000a8bff219e9253e654f1cd90fca
Dev786s <85985141+Dev786s@users.noreply.github.com>
2022-03-30 02:52:15 -0700

    Update mt6768-common.mk

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -308,11 +308,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# TinyXML
-PRODUCT_PACKAGES += \
-    libtinyxml \
-    libqdMetaData \
-    libqdMetaData.system
 # Camera
 PRODUCT_PACKAGES += \
     GoogleCameraGo
